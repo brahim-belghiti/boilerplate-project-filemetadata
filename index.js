8713262833326c6e582c7152c846e5fd79ab42bd
brahim-belghiti <brahim.belghiti@proton.me>
2024-04-25 23:50:00 +0100

    connect to mongo

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+const appRoutes = require('./src/routes/routes');
+
+// mongo 
+const DATA_BASE_URI = process.env;
+const mongoose = require('mongoose');
+mongoose.connect(DATA_BASE_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  console.log('Database connection successful');
+})
+  .catch((err) => {
+    console.error('Database connection error');
+  });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +22,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// routes
+app.use('/api', appRoutes);
 
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8001;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
